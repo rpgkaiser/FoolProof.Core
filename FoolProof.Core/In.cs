6f7838a5d6c9cf352bd23c8436aefc7489c870b9
Rafael Peña García <rafael.pena@daktronics.com>
2019-06-10 11:16:35 -0400

    Added In and NotIn validation attributes.

---
+++ FoolProof.Core/In.cs
@@ -0,0 +1,8 @@
+﻿
+namespace FoolProof.Core
+{
+    public class InAttribute : IsAttribute
+    {
+        public InAttribute(string dependentProperty) : base(Operator.In, dependentProperty) { }
+    }
+}
---
+++ FoolProof.Core/NotIn.cs
@@ -0,0 +1,8 @@
+﻿
+namespace FoolProof.Core
+{
+    public class NotInAttribute : IsAttribute
+    {
+        public NotInAttribute(string dependentProperty) : base(Operator.NotIn, dependentProperty) { }
+    }
+}
